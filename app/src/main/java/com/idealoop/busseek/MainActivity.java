5186bcfd7fee4eb21067a6d8ab6ec04e199c456f
neoPerera <chanuthnilushan@gmail.com>
2020-06-14 12:33:33 +0530

    test commit

--- app/src/main/java/com/idealoop/busseek/MainActivity.java
+++ app/src/main/java/com/idealoop/busseek/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        //test  from neo
+        int a = 1;
+        //end test
     }
 }
